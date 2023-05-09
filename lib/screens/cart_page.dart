2b5ca630871a41f5965000f55902f5c0f8ef6eee
jaythanki18 <21CE143@charusat.edu.in>
2023-05-10 00:37:03 +0530

    day 29 completed

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+import '../core/store.dart';
+
 class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,35 +28,27 @@ class CartPage extends StatelessWidget {
 }
 
 class _CartTotal extends StatelessWidget {
+
+  // const _CartTotal({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final CartModel _cart = (VxState.store as MyStore).cart;
+    final CartModel _cart=(VxState.store as MyStore).cart;
     return SizedBox(
       height: 200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          VxBuilder(
-            mutations: {RemoveMutation},
-            builder: (context, _) {
-              return "\$${_cart.totalPrice}"
-                  .text
-                  .xl5
-                  .color(context.theme.accentColor)
-                  .make();
-            },
-          ),
+          "\$${_cart.totalPrice}".text.xl5.color(context.theme.accentColor).make(),
           30.widthBox,
           ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: "Buying not supported yet.".text.make(),
-              ));
-            },
-            style: ButtonStyle(
-                backgroundColor:
-                MaterialStateProperty.all(context.theme.buttonColor)),
-            child: "Buy".text.white.make(),
+              onPressed: (){
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: "Buying not suppoted yet.".text.make()));
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(context.theme.buttonColor)
+              ),
+              child: "Buy".text.white.make()
           ).w32(context)
         ],
       ),
@@ -60,23 +56,22 @@ class _CartTotal extends StatelessWidget {
   }
 }
 
-class _CartList extends StatelessWidget {
+class _CartList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    VxState.watch(context, on: [RemoveMutation]);
-    final CartModel _cart = (VxState.store as MyStore).cart;
-    return _cart.items.isEmpty
-        ? "Nothing to show".text.xl3.makeCentered()
-        : ListView.builder(
+    VxState.watch(context,on:[RemoveMutation]);
+    final CartModel _cart=(VxState.store as MyStore).cart;
+    return _cart.items.isEmpty ? "Nothing to show".text.xl3.makeCentered():ListView.builder(
       itemCount: _cart.items?.length,
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
           icon: Icon(Icons.remove_circle_outline),
-          onPressed: () => RemoveMutation(_cart.items[index]),
+          onPressed: ()=>RemoveMutation(_cart.items[index]),
         ),
         title: _cart.items[index].name.text.make(),
       ),
     );
   }
 }
+
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
@@ -8,7 +9,7 @@ import 'dart:convert';
 import 'package:velocity_x/velocity_x.dart';
 import '../widgets/home_widgets/catalog_header.dart';
 import '../widgets/home_widgets/catalog_list.dart';
-
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -19,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   final int days = 30;
   final String name = "Codepur";
 
+  final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
   @override
   void initState() {
     // TODO: implement initState
@@ -27,8 +29,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-   // await Future.delayed(Duration(seconds: 2));
-    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+
+    // final response=await http.get(Uri.parse(url));
+    // final catalogJson=response.body;
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
@@ -39,13 +45,40 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
       backgroundColor: context.canvasColor,
       floatingActionButton: FloatingActionButton(
-        onPressed: ()=>Navigator.pushNamed(context, MyRoutes.cartRoute),
+        onPressed: (){
+          Navigator.pushNamed(context, MyRoutes.cartRoute);
+        },
         backgroundColor: MyTheme.darkBluishColor,
         child: Icon(CupertinoIcons.cart),
+      ).badge(
+          color: Vx.red500,
+          size: 20,
+          count: _cart.items.length,
+          textStyle: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          )
       ),
+
+      // floatingActionButton: VxBuilder(
+      //   mutations: {AddMutation, RemoveMutation},
+      //   builder: (ctx, _) => FloatingActionButton(
+      //     onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+      //     backgroundColor: MyTheme.darkBluishColor,
+      //     child: Icon(CupertinoIcons.cart),
+      //   ).badge(
+      //       color: Vx.red500,
+      //       size: 20,
+      //       count: _cart.items.length,
+      //       textStyle: TextStyle(
+      //         color: Colors.black,
+      //         fontWeight: FontWeight.bold,
+      //       )),
+      // ),
       body: SafeArea(
         child: Container(
             padding: Vx.m32,
@@ -65,7 +98,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
-
-
